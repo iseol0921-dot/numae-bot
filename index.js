0bccd86c3dd961eef9bee308ae2d94e2071b1995
iseol0921-dot <iseol0921@gmail.com>
2026-06-24 08:12:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
