27bb4abc3d8ff179804bb0a87811c6d9fa61df80
iseol0921-dot <iseol0921@gmail.com>
2026-06-24 07:48:27 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} 로그인 완료!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
